bb31312f2b760468bb08d6abb8b00f76fab569e7
Mehrabd20 <mehrabd20@gamil.com>
2023-12-17 15:54:56 +0330

    add react-hook-form

--- src/ui/RadioInput.jsx
+++ src/ui/RadioInput.jsx
@@ -1,17 +1,27 @@
-const RadioInput = ({ id, name, checked, value, onChange, label }) => {
+const RadioInput = ({
+   name,
+   value,
+   label,
+   watch,
+   register,
+   required,
+   validationSchema,
+}) => {
    return (
       <div className="flex items-center justify-center">
          <input
             type="radio"
             name={name}
-            id={id}
-            checked={checked}
+            id={value}
+            checked={watch(name) === value}
             value={value}
-            onChange={onChange}
+            {...register(name, validationSchema)}
             className="cursor-pointer form-radio rounded-full border-2 border-secondary-400 bg-transparent
             w-4 h-4 checked:text-primary-900 ml-2"
          />
-         <label htmlFor={id}>{label}</label>
+         <label htmlFor={value}>
+            {label} {required && <span className="text-error">*</span>}
+         </label>
       </div>
    );
 };
--- src/ui/TextField.jsx
+++ src/ui/TextField.jsx
@@ -1,24 +1,35 @@
-const TextField = ({ value, onChange, name, label, type = "text" }) => {
+const TextField = ({
+   register,
+   name,
+   label,
+   type = "text",
+   required,
+   validationSchema,
+   errors,
+}) => {
    const placeholder = "..." + label;
 
    return (
       <div>
          <label
             htmlFor={name}
-            className="mb-2 block text-lg font-bold">
-            {label}
+            className="mb-2 block text-base font-bold text-secondary-700">
+            {label} {required && <span className="text-error">*</span>}
          </label>
          <input
-            value={value}
-            onChange={onChange}
+            {...register(name, validationSchema)}
             type={type}
             id={name}
-            name={name}
             className="textField__input"
             autoComplete="off"
             placeholder={placeholder}
             dir="auto"
          />
+         {errors && errors[name] && (
+            <span className="text-error block text-sm mt-2">
+               {errors[name]?.message}
+            </span>
+         )}
       </div>
    );
 };
