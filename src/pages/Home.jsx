28d7b6d3e2bf00eb02fe357ef6c006352cca1bf7
mehrabFronto <mehrabd20@gmail.com>
2024-05-20 19:11:51 +0330

    refactore: home page content has been changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import Loading from "../ui/Loading";
+
 const Home = () => {
    return (
-      <div className="container text-primary-900 text-3xl font-bold p-4">
-         صفحه اصلی
+      <div className="container text-primary-900 text-3xl font-bold p-4 h-full 
+      flex flex-col items-center justify-center">
+         <p className="text-center ml-1">  
+            حساب شما در انتظار تایید است
+         </p>
+         <Loading />
       </div>
    );
 };
