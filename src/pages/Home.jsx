37bf575b508264e8decc4d3ece97e186f4125919
mehrabFronto <mehrabd20@gmail.com>
2024-05-20 19:12:36 +0330

    fix: redirection in home page has been fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import Loading from "../ui/Loading";
+import { useNavigate } from "react-router-dom";
+import useAuthorize from "../feature/authentication/useAuthorize";
 
 const Home = () => {
+   const navigate = useNavigate();
+
+   const { user, isVerified, isAuthenticated } = useAuthorize();
+
+   useEffect(() => {
+      if(!isAuthenticated){
+         navigate('/auth', { replace: true });
+      }
+      else if(isAuthenticated && isVerified){
+          navigate(`/${user.role.toLowerCase()}`, { replace: true });
+      }
+   }, [user, isVerified, isAuthenticated, navigate]);
+
    return (
       <div className="container text-primary-900 text-3xl font-bold p-4 h-full 
       flex flex-col items-center justify-center">
